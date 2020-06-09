8e0903bed650c089344ac52c668a4ba65529eba3
Juan Madalena <juanmadalena06@gmail.com>
2020-06-09 00:43:08 +0000

    Jmadalena

--- FRONT-END/src/pages/GetStarted/GetStarted.jsx
+++ FRONT-END/src/pages/GetStarted/GetStarted.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
+import world from "../../shared/img/world@3x.png";
+import './GetStarted.scss'
 
 export function GetStarted () {
 
     return(
-        <>
-            <h1>El mismo precio en cualquier parte</h1>
-            <a href="https://google.com" ><button className="b-btn">Empezar YA</button></a>
-        </>
+        <div className="p-welcome">
+            <div className="p-welcome__container">
+                <img src={world} alt="/" className="p-get-started__img" />
+                <div className="p-welcome__text">
+                    <p className="title-primary">El mismo precio en cualquier parte</p>
+                </div>
+                <div className="p-welcome__text">
+                    <p className="text-secondary">Dispondrás de un precio fijo estés donde estés, sin importar el tamaño o el pesp.</p>
+                </div>
+                <div className="p-welcome__btn">
+                    <a href="/"><button className="b-btn">Empezar Ya</button></a>
+                    <a href="/" className="p-get-started__link"><p>Consulta precios</p></a>
+                </div>
+            </div>
+        </div>
     )
 
 }
