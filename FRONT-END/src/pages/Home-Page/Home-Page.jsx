ce3fe88c7cfe41d627b3ab8aab1dcb8c589a7769
Pedro Vizeu <pedro.vizeu.pinheiro@gmail.com>
2020-06-09 21:24:12 +0200

    actualizacion de conflictos

--- FRONT-END/src/pages/Home-Page/Home-Page.jsx
+++ FRONT-END/src/pages/Home-Page/Home-Page.jsx
@@ -5,11 +5,21 @@ import {NavComponent} from "../../shared/components/Nav-Component/Nav-Component"
 
 export function HomePage() {
 
+    const exp = [{
+        title : "Un pedazito de italia",
+        descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc porta lacus a vestibulum rhoncus. Nulla vehicula ligula eget mollis mattis. Duis pellentesque tincidunt feugiat ",
+        img : "https://www.ef.com/sitecore/__/~/media/universal/pg/8x5/destination/IT_00_00_1.jpg",
+    },{
+        title : "Visita Barcelona",
+        descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc porta lacus a vestibulum rhoncus. Nulla vehicula ligula eget mollis mattis. Duis pellentesque tincidunt feugiat ",
+        img : "https://gd-consejosdeespana.sfo2.cdn.digitaloceanspaces.com//2015/05/Sagrada-Familia-Barcelona.jpg",
+    }]
+
     return(
         <div>
         <h1>Home</h1>
             <NewsComponent/>
-            <ExperienceComponent/>
+            <ExperienceComponent exp={exp}/>
             <NavComponent/>
         </div>
     )
--- FRONT-END/src/shared/components/NewsComponent/NewsComponent.jsx
+++ FRONT-END/src/shared/components/NewsComponent/NewsComponent.jsx
@@ -9,14 +9,16 @@ export function NewsComponent() {
             </div>
             <div className="c-news__overflow">
             <div className="c-news__item">
-
+                <div>
+                    <img src="https://media.nomadicmatt.com/2019/travelfreedom2.jpg" alt="/" className="c-news__img"/>
+                </div>
             </div>
-                <div className="c-news__item">
+                    <div className="c-news__item">
+                        <div>
+                            <img src="https://image.freepik.com/free-photo/asian-man-life-jacket-hands-up-happy-travel-freedom-journey-lifestyle-concept_1428-1267.jpg" alt="/" className="c-news__img"/>
+                        </div>
 
-                </div>
-                <div className="c-news__item">
-
-                </div>
+                    </div>
                 <div className="final"></div>
             </div>
         </div>
