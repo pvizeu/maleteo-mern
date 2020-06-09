751045e6643ad9c4662179c6dbc454e22d1eed16
@zonarolo <luis.rolo@bootcamp-upgrade.com>
2020-06-09 02:10:25 +0200

    Error in git

--- FRONT-END/src/App.jsx
+++ FRONT-END/src/App.jsx
@@ -1,11 +1,26 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Logo } from './pages/Logo/Logo';
+import { Welcome } from './pages/Welcome/Welcome';
+import { GetStarted } from './pages/GetStarted/GetStarted';
 
 function App() {
   return (
-    <div>
-      <p>Hello world</p>
-    </div>
+    <Router>
+
+      <Switch>
+        <Route path="/paso2">
+          <GetStarted/>
+        </Route>
+        <Route path="/paso1">
+          <Welcome/>
+        </Route>
+        <Route path="/">
+          <Logo/>
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
