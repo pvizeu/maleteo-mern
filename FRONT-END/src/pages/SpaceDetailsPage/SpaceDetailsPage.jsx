e5aa584f7f1132a53798cfd4b2d52d1c63501d8c
@zonarolo <luis.rolo@bootcamp-upgrade.com>
2020-06-17 05:04:08 +0200

    Creating location page

--- FRONT-END/src/pages/SpaceDetailsPage/SpaceDetailsPage.jsx
+++ FRONT-END/src/pages/SpaceDetailsPage/SpaceDetailsPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import axios from 'axios';
 import { SliderComponent } from '../../shared/components/SliderComponent/SliderComponent';
 import { useEffect } from 'react';
-import {environment} from "../../../environments/environment";
+import {environment} from "../../environments/environment";
 
 
 export function SpaceDetailsPage() {
@@ -13,21 +13,28 @@ export function SpaceDetailsPage() {
 
     }, [] )
 
+  const space = {
+    alias: "Marta1",
+    availability: "Abierto 24h",
+    createdAt: "2020-06-14T20:04:25.308Z",
+    discount: 0,
+    email: "pedro@mail.com",
+    latitud: 40.417866,
+    localization: "xxxx1ssss",
+    locker: "El hall es un sitio espacioso y limpio que fue modificado 222",
+    longitud: -3.709394,
+    photos: (2) [["https://res.cloudinary.com/dqp7c3bnr/image/upload/v1592238573/espacios-guardianes/alojamientoCopy_3x_gx6qwm.png"],["https://res.cloudinary.com/dqp7c3bnr/image/upload/v1592238573/espacios-guardianes/living-room-2583032_640_rhsqwa.jpg"]],
+    property: "casa",
+    services: "bajamos de 10 a 4 euros mas llevamos a donde quiera dentro de la M30",
+    space: "hall",
+    title: "ESPACIO 1",
+    updatedAt: "2020-06-15T13:09:05.020Z",
+    __v: 0,
+    _id: "5ee682c9621868ad754163ef",
+  }
     return(
         <div>
-            <SliderComponent guardian={guardian} />
-            GuardianDetails
-            -title
-            -direction
-            -name
-            -photo
-            Tipo de Locker
-            RoomDetails
-            StaticMap
-            OrderDetails
-            Rules
-            Cost
-            Help
+            <SliderComponent info={space} />
         </div>
     )
 }
--- FRONT-END/src/shared/components/SliderComponent/SliderComponent.jsx
+++ FRONT-END/src/shared/components/SliderComponent/SliderComponent.jsx
@@ -2,10 +2,88 @@ import React from 'react';
 
 
 export function SliderComponent (props) {
-
-
+    console.log(props.info);
 
     return(
-        <h1>H</h1>
+      <>
+          <p>{props.info.alias}</p>
+          <p>Tetuan, Madrid</p>
+          <p>{props.info.availability}</p>
+          <p>{props.info.createdAt}</p>
+          <p>{props.info.discount}</p>
+          <p>{props.info.email}</p>
+          <p>{props.info.latitud}</p>
+          <p>{props.info.longitud}</p>
+          <p>{props.info.localization}</p>
+          <p>{props.info.locker}</p>
+          <p>{props.info.property}</p>
+          <p>{props.info.services}</p>
+          <p>{props.info.space}</p>
+          <p>{props.info.title}</p>
+          <p>{props.info._id}</p>
+        <hr/>
+          <div>
+              <div>
+                <img src="" alt=""/>
+                <p>Tipo de locker</p>
+                <p>El salón de su casa será el lugar idóneo para alojar tus maletas y cuidar de ellas.</p>
+              </div>
+              <div>
+                <img src="" alt=""/>
+                <p>Como la patena</p>
+                <p>19 usuarios recientes han catalogado su espacio como muy limpo.</p>
+              </div>
+              <div>
+                <img src="" alt=""/>
+                <p>Un Fortín</p>
+                <p>el 95% de los usuarios han valorado su experiéncia como muy segura.</p>
+              </div>
+          </div>
+          <hr/><p>Habitación espaciada a 15 minutos del centro de madrid y a 5 minutos del la Linea 1. Además ofrezco información turística personalizada.</p><hr/>
+          <div>
+              <p>Ubicacion</p>
+              <img src="https://storage.googleapis.com/support-forums-api/attachment/thread-9014924-11470506657998028469.JPG" alt=""/>
+          </div>
+          <div>
+              <p className="b-title">Reseñas</p>
+              <div>
+                  <img src="https://res.cloudinary.com/dqp7c3bnr/image/upload/v1592238573/espacios-guardianes/living-room-2583032_640_rhsqwa.jpg" alt=""/>
+                  <p>{props.info.alias}</p>
+                  <p>En Julio de 2019</p>
+                  <p>{props.info.locker}</p>
+              </div>
+              <div>
+                  <img src="https://res.cloudinary.com/dqp7c3bnr/image/upload/v1592238573/espacios-guardianes/living-room-2583032_640_rhsqwa.jpg" alt=""/>
+                  <p>{props.info.alias}</p>
+                  <p>En Julio de 2019</p>
+                  <p>{props.info.locker}</p>
+              </div>
+              <div>
+                  <img src="https://res.cloudinary.com/dqp7c3bnr/image/upload/v1592238573/espacios-guardianes/living-room-2583032_640_rhsqwa.jpg" alt=""/>
+                  <p>{props.info.alias}</p>
+                  <p>En Julio de 2019</p>
+                  <p>{props.info.locker}</p>
+              </div>
+          </div>
+          <div>
+              <p className="b-title">Normas de Marta</p>
+              <p>Cómo debe ser tu maleta</p><hr/>
+              <p>Tipo de cancelación de reserva</p><hr/>
+              <p>Contactar con tu guardián</p><hr/>
+              <p>Denunciar anuncio</p><hr/>
+          </div>
+          <div>
+              <p className="b-title">Otros lockers cerca de ti</p>
+              {/*<img src="" alt=""/>*/}
+              {/*<img src="" alt=""/>*/}
+          </div>
+
+          <div>
+              <p>Total <strong>12 $</strong></p>
+              <a href="">Desglose del precio</a>
+              <button className="b-btn">Reservar Ahora</button>
+          </div>
+      </>
+
     )
 }
