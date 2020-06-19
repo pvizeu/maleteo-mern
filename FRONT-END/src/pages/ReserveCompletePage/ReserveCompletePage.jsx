0e4a1bf5dc8559097e5e7780de84c11c646139e6
@zonarolo <luis.rolo@bootcamp-upgrade.com>
2020-06-19 03:40:24 +0200

    Working in reserveDetailsPage

--- FRONT-END/src/pages/ReserveCompletePage/ReserveCompletePage.jsx
+++ FRONT-END/src/pages/ReserveCompletePage/ReserveCompletePage.jsx
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, {useState} from "react";
 import './ReserveCompletePage.scss';
 import {CloseComponent} from "../../shared/components/CloseComponent/CloseComponent";
 import fenix from '../../shared/img/fenix@3x.png';
+import {useLocation} from "react-router-dom";
 
 export function ReserveCompletePage() {
+
+  // Funcion necesaria para hacer uso de queryParams
+  function useQuery() {
+    return new URLSearchParams(useLocation().search);
+  }
+  let query = useQuery();
+
+  const [navigation, setNavigation] = useState({
+    latitude: (query.get("latitude") ? query.get("latitude") : ""),
+    longitude: (query.get("longitude") ? query.get("longitude") : ""),
+    localization: (query.get("localization") ? query.get("localization") : ""),
+    deliver: (query.get("deliver") ? query.get("deliver") : ""),
+    removal: (query.get("removal") ? query.get("removal") : ""),
+    pieces: (query.get("pieces") ? query.get("pieces") : ""),
+    url: "location",
+    useremail: (query.get("useremail") ? query.get("useremail") : ""),
+    guardianemail: (query.get("guardianemail") ? query.get("guardianemail") : ""),
+    title: (query.get("title") ? query.get("title") : ""),
+    spacetitle: (query.get("spacetitle") ? query.get("spacetitle") : ""),
+    discount: (query.get("discount") ? query.get("discount") : ""),
+    preciosindiscount: (query.get("preciosindiscount") ? query.get("preciosindiscount") : "")
+  });
 
   return (
     <div className=" p-4 complete">
